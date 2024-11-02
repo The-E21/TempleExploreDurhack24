f08df19d997a2b791de76ec91edf59a4911b01da
The-E21 <me@email.com>
2024-11-02 15:52:49 +0000

    Added a jump

--- Scripts/playerscript.gd
+++ Scripts/playerscript.gd
@@ -1,17 +1,67 @@
 extends CharacterBody2D
-@export var speed : float
-@export var gravity : float = 9.8
 
 #Horizontal movement
 var leftRightInput : float
 var leftRightAimVelocity : float
+@export var speed : float
 @export var accelerationMultiplier : float
+
+#Vertical movement
+var jumpDown : bool = false
+@export var gravity : float = 9.8
+var jumpPhase : int
+@export var jumpHight : float
+@export var apexPercentile : float
+@export var jumpCut : float
+
+@export var jumpTime : float
+@export var apexTime : float
+var jumpTimer : float
+
+func _input(event):
+	if event.is_action_pressed("jump"):
+		jumpDown = true
+	elif event.is_action_released("jump"):
+		jumpDown = false
 
 func  _process(delta):
 	leftRightInput = Input.get_axis("ui_left", "ui_right")
 	
 func _physics_process(delta):
-	velocity += Vector2.DOWN * gravity * delta
+	manageJump(delta)	
+	##Horizontal
 	leftRightAimVelocity = leftRightInput * speed
 	velocity += Vector2((leftRightAimVelocity - velocity.x) * accelerationMultiplier * delta, 0)
 	move_and_slide()
+
+func manageJump(delta):
+	match jumpPhase:
+		0:
+			if(not is_on_floor()):
+				jumpPhase = 3
+			if(jumpDown):
+				print("go")
+				jumpPhase = 1
+				jumpTimer = 0
+		1:
+			var jumpSeed = jumpHight * apexPercentile / jumpTime
+			velocity = Vector2(velocity.x, -jumpSeed)
+			jumpTimer += delta
+			
+			if(not jumpDown):
+				jumpPhase = 3
+				velocity = Vector2(velocity.x, velocity.y * jumpCut)
+				
+			elif jumpTimer >= jumpTime:
+				jumpPhase = 2
+				jumpTimer = 0
+				velocity = Vector2(velocity.x, 2 * (1 - apexPercentile) * jumpHight / (apexTime/2))
+		2:
+			velocity -= Vector2.DOWN * 2*(1 - apexPercentile) * jumpHight / pow(apexTime / 2, 2)
+			jumpTimer += delta
+			jumpPhase = 3
+		3:
+			velocity += Vector2.DOWN * gravity * delta
+			if(is_on_floor()):
+				jumpPhase = 0
+	
