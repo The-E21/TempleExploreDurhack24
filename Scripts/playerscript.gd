70b89ab1bbc9e35b703a43e746e4aac12a431807
The-E21 <me@email.com>
2024-11-02 14:39:32 +0000

    Added horizontal movement

--- Scripts/playerscript.gd
+++ Scripts/playerscript.gd
@@ -2,6 +2,17 @@ extends CharacterBody2D
 @export var speed : float
 @export var gravity : float = 9.8
 
+#Horizontal movement
+var leftRightInput : float
+var leftRightAimVelocity : float
+@export var accelerationMultiplier : float
+
+func  _process(delta):
+	leftRightInput = Input.get_axis("ui_left", "ui_right")
+	
 func _physics_process(delta):
 	velocity += Vector2.DOWN * gravity * delta
+	leftRightAimVelocity = leftRightInput * speed
+	velocity += Vector2((leftRightAimVelocity - velocity.x) * accelerationMultiplier * delta, 0)
+	print(velocity.x)
 	move_and_slide()
