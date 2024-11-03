0f96edc158ad1b33106b09da0b904e47ac537148
The-E21 <me@email.com>
2024-11-03 01:06:53 +0000

    Added sound when button pressed

--- Scripts/button.gd
+++ Scripts/button.gd
@@ -8,6 +8,7 @@ var touching : bool
 func _input(event):
 	if(event.is_action_pressed("interact") and touching):
 		press.emit(id)
+		$AudioStreamPlayer.play()
 
 func _on_body_entered(body):
 	touching = true
