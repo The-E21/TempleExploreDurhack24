f2c643b75c3a0eb6f1e84aa0c7bab33704be00b2
The-E21 <me@email.com>
2024-11-03 03:31:46 +0000

    Packed text popup into scene

--- Testing/animate_text.gd
+++ Testing/animate_text.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+@export var text : String
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Area2D.connect("body_entered", Callable(self, "play_animation")) 
 	$Label.visible = false
+	$Label.text = text
+	
 func play_animation(body):
 	$Area2D.disconnect("body_entered", Callable(self, "play_animation"))
 	$Label.visible = true
